1d73045cdc8f578e780566a86b51b1dc9785aae7
Parasyte127 <113103528+Parasyte127@users.noreply.github.com>
2022-09-20 10:22:41 +0100

    Jump added

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public GameObject groundObject;
+    public GameObject groundChecker;
     public LayerMask whatIsGround; 
 
-    float maxSpeed = 5.0f;
-    bool isOnGround= false;  
+    public float maxSpeed = 5.0f;
+    public float jumpForce = 100.0f;
+    public bool isOnGround= false;  
     //reference to Rigid body to allow it to be maipulated
     Rigidbody2D playerObject;
+    float movementValueX = 0.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -23,12 +25,20 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         //create afloat that will be equal to the player's horizontal input
-        float movementValueX = Input.GetAxis("Horizontal");
+        movementValueX = Input.GetAxis("Horizontal");
 
         //Change the X velocity of the Rigidbody2D to be equal tothe movement value
-        playerObject.velocity = new Vector2(movementValueX,playerObject.velocity.y);
+        playerObject.velocity = new Vector2(movementValueX * maxSpeed,playerObject.velocity.y);
 
         isOnGround = Physics2D.OverlapCircle(groundChecker.transform.position, 1.0f, whatIsGround);
         
+        if (Input.GetKeyDown(KeyCode.Space) &&  isOnGround == true)
+        {
+            playerObject.AddForce(new Vector2(0.0f, 2.5f) * jumpForce);
+        }
+        if (Input.GetKeyDown(KeyCode.Space) &&  isOnGround == false)
+        {
+            playerObject.AddForce(new Vector2(0.0f, 100.0f));
+        }
     }
 }
